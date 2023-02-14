d7019d143d231b6943c7342228a8adf95299d6ec
hhyeok1026 <hhyeok1026@gmail.com>
2023-02-14 12:55:48 +0900

    시간 많이 걸려서 풀긴했는데, 다른사람은 정규식 써서 3줄로 짰다...;; 짬바의 차이가..  크다

--- src/main/kotlin/baekjoon/string/2941.kt
+++ src/main/kotlin/baekjoon/string/2941.kt
@@ -17,38 +17,73 @@ output
 dz=ak
 특정문자들이 크로아티아 알파벳인지 확인하고 -> 아니면, 그냥 알파벳으로 쳐야함.
 문자열의 앞과 뒤를 체크할 수 있어야하는 건가?
- */
-/*
-저 테이블에 대한 문자열들을 뽑아서 파싱하고,
-나머지는 그냥 1개단어로 치면 되는 문제네.
+
+--------------------
+
+해당 문자열들 찾고, 여러번 반복해서 없을때까지하고
+최후에 남은 문자열을 갯수 세서 +
+이중 반복문을 써야하나.. do-while문을 쓰면 되려나?
+일단 쉽게 1개 찾아서 빼보자.
+
+이것도 생각보다 어려웠고 다른사람들 짜둔거를 봐야할듯
+
+dz=d-d-dz=dddz=z=s=z=z
+답: 11
+
+dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=dz=d
+답: 34
+
+z=z=dz=dz=ddz=ddz=dz=dz=z=z=
+답: 12
+
 */
 
 fun main(args: Array<String>) {
 
-    val input = readln()
-    var count = 0
+    var input = readLine()!!
+    var croatianAlphabetCount = 0
 
     val croatianAlphabetList = arrayOf("c=", "c-", "dz=", "d-" ,"lj", "nj", "s=" ,"z=")
     // println(croatianAlphabetList.contentToString())
 
+    // 크로아티아 알파벳 제거 및 찾는 로직
     do {
+
         var existCroatian = false
 
-        croatianAlphabetList.forEach {
+        // 이 리스트들 사이에서도 우선순위가 있어서, 계속 돌리면 안되고,
+        // 하나 찾고나면 break를 해줘야함.
+        for (it in croatianAlphabetList) {
             if (input.contains(it)) {
                 existCroatian = true
-                
-                // 1. 해당 문자열을 input에서 삭제해내고, 카운트 1올리기
-                count++
+                croatianAlphabetCount++
+
+                // 해당 문자열을 input에서 삭제해내고, 카운트 1올리기
+                // println("$input ($it)")
+
+                // 공백없이 replace를 하면, 중간에 문자열이 없어지면, 다시 합쳐진거 처럼됨. 그래서 크로아티아 알파벳 추가로 생겨 버그가 생김
+                // 그래서 " "로 대체시키고 뒤쪽 로직에서 다시 처리함.
+                input = input.replaceFirst(it, " ")
+                break
             }
         }
+    } while (existCroatian)
+    // println(input)
+    // println(croatianAlphabetCount)
 
-    } while (existCroatian) // 내부에 foreach로 돌리지만, 계속해서 있을 수 있기때문에 while문을 붙임.
+    // 크로아티아 알파벳이외의 알파벳 추가해야함.
+    // 버그가 안생기게 수정해야함.
+    var otherAlphabets = input.split(" ").filter { it != "" }.joinToString("")
 
-    // 해당 문자열들 찾고, 서브스트링, 여러번 반복해서 없을때까지하고
-    // 최후에 남은 문자열을 갯수 세서 +
-    // 이중 반복문을 써야하나.. // do-while문을 쓰면 되려나?
-    // 일단 쉽게 1개 찾아서 빼보자.
-    println(count)
+    /*otherAlphabets.forEach {
+        println(it)
+    }*/
+    // println(otherAlphabets)
+    otherAlphabets = otherAlphabets.replace(regex = "[-=]".toRegex(), replacement = "")
+    // println(otherAlphabets)
+
+    val otherAlphabetCount = otherAlphabets.length
+
+    println(croatianAlphabetCount + otherAlphabetCount)
 }
 
