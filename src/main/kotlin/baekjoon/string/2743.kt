b2815b940130adb0b58035e8b59e8e46b464c5af
hhyeok1026 <hhyeok1026@gmail.com>
2023-02-20 22:33:27 +0900

    오늘 집에서 아주 쉬운 문제 하나 풀었다. 맥에서도 윈도우와 같이 jdk17다운받아서 지정하니, 파일 수정이 안되었다. 그래도 개발환경이 다르면,, 그냥.. 해당 파일을 제외하고 커밋하는게 나은듯하다.

---
+++ src/main/kotlin/baekjoon/string/2743.kt
@@ -0,0 +1,21 @@
+package baekjoon.string
+
+/*
+알파벳으로 이루어진 단어를 입력받아,
+그 길이를 출력하는 프로그램을 작성
+
+input
+- 영어 소문자와, 대문자로 이루어진 단어, (length는 ~100)
+
+ouput
+- 첫째줄에 입력으로 주어진 단어의 길이를 출력
+
+문제가 너무 쉬운데..? 오늘 문제 쉽게 하나 풀었다.!
+ */
+
+fun main(args: Array<String>) {
+
+    val input = readln()
+
+    println(input.length)
+}
